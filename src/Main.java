78dd6e01bd20e4672cc4c75027ee07f23f1848f4
Alexeuss82 <krukovalex@gmail.com>
2022-10-21 15:00:19 +0300

    корректный расчет

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int amountTicket = 1000;
-        int oneBonusMill = amountTicket / 20;
+        int amountTicket = 10_000;
+        int oneBonusMill = 20;
         int ticketBonusMills = amountTicket / oneBonusMill;
         System.out.println("Вам начислено " + ticketBonusMills + " бонусных милль!");
     }
